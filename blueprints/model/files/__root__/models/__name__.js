c01c7cc2cd86267bf4a8422ac2aa579cb6240e54
Randall Morey <randall@randallmorey.com>
2018-05-19 09:14:53 -0400

    chore(blueprint): add relationship examples

--- blueprints/model/files/__root__/models/__name__.js
+++ blueprints/model/files/__root__/models/__name__.js
@@ -1,5 +1,7 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
+// import { belongsTo } from 'ember-data/relationships';
+// import { hasMany } from 'ember-data/relationships';
 import Validator from '../mixins/object-validator';
 
 /**
@@ -14,6 +16,7 @@ const <%= className %> = Model.extend(Validator, {
   // =attributes
 
   //example: attr('string'),
+  //exampleRelationship: belongsTo('organization'),
   updated_at: attr('date'),
   created_at: attr('date'),
 
