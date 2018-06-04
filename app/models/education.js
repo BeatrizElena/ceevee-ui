19c7a2cb7e4f32323c163a8931efc5ce0ebce495
Randall Morey <randall@randallmorey.com>
2018-06-04 19:10:28 -0400

    feat(education): add education model
    closes #4

---
+++ app/models/education.js
@@ -0,0 +1,43 @@
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { belongsTo } from 'ember-data/relationships';
+import Validator from '../mixins/object-validator';
+
+/**
+ * Model description goes here.
+ * @class
+ * @augments ember-data/model
+ * @augments ember-model-validator/mixins/object-validator
+ * @property name {String} required
+ * @property credential {String} required
+ * @property organization {Organization} required
+ * @property completed_on {Date}
+ * @property updated_at {Date}
+ * @property created_at {Date}
+ */
+const Education = Model.extend(Validator, {
+
+  // =attributes
+
+  name: attr('string'),
+  credential: attr('string'),
+  description: attr('string'),
+  organization: belongsTo('organization'),
+  completed_on: attr('date'),
+  updated_at: attr('date'),
+  created_at: attr('date'),
+
+  // =validations
+
+  /**
+   * @memberof Education
+   * @see {@link https://github.com/esbanarango/ember-model-validator}
+   */
+  // eslint-disable-next-line ember/avoid-leaking-state-in-ember-objects
+  validations: {
+    name: {presence: true},
+    credential: {presence: true}
+  }
+});
+
+export default Education;
